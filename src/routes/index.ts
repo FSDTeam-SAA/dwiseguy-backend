ab1612a79da04ced0f2ddeb0ef7c46566a40e399
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2026-01-06 05:05:34 +0600

    after merge (conflict)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express';
 import userRoutes from '../modules/user/user.routes';
 import courseRoutes from '../modules/course/course.routes';
-<<<<<<< HEAD
 import quizRoutes from '../modules/quiz/quiz.route';
 import quizAttemptRoutes from '../modules/quiz/quizAttempt.route';
-=======
->>>>>>> daf9a9a2ba4fdcf4ae17e70aaa7596e96422c6bf
 import lessonRouter from '../modules/lesson/lesson.route';
 import { subLessonRouter } from '../modules/sublesson/sublesson.route';
 import { progressRouter } from '../modules/progress/progress.route';
@@ -21,7 +18,6 @@ const moduleRoutes = [
             route: courseRoutes,
       },
       {
-<<<<<<< HEAD
             path: '/quiz',
             route: quizRoutes,
       },
@@ -29,19 +25,6 @@ const moduleRoutes = [
             path: '/quiz/student',
             route: quizAttemptRoutes,
       },
-=======
-            path: '/admin/lesson',
-            route: lessonRouter,
-      },
-      {
-    path: '/admin/sublesson',
-    route: subLessonRouter,
-  },
-  {
-    path: '/user/progress',
-    route: progressRouter,
-  },
->>>>>>> daf9a9a2ba4fdcf4ae17e70aaa7596e96422c6bf
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
