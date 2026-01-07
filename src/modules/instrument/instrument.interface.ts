80e226786e29072a10ff849121dd7cdb92ede8cb
sabbir <sabbir00921@gmail.com>
2026-01-08 04:55:35 +0600

    update course v.3

--- src/modules/instrument/instrument.interface.ts
+++ src/modules/instrument/instrument.interface.ts
@@ -1,6 +1,6 @@
 import { Document, Model, Types, Schema } from 'mongoose';
 import { ILesson } from '../lesson/lesson.interface';
-import { ISublesson } from '../sublesson/sublesson.interface';
+import { IModule } from '../module/module.interface';
 
 export interface Iinstrument extends Document {
       _id: string;
@@ -23,7 +23,7 @@ export type TCreateInstrument = {
       level?: string;
 };
 
-export type PopulatedSubLesson = ISublesson & {
+export type PopulatedModule = IModule & {
       media?: {
             images?: { public_id: string; url: string }[];
             audio?: { public_id: string; url: string };
@@ -32,7 +32,7 @@ export type PopulatedSubLesson = ISublesson & {
 
 export type PopulatedLesson = ILesson & {
       images?: { public_id: string; url: string }[];
-      sublessons?: PopulatedSubLesson[];
+      sublessons?: PopulatedModule[];
 };
 
 export type PopulatedInstrument = Iinstrument & {
