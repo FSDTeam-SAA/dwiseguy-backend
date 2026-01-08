39fef34b90d912cbe88db35e20caf5c78083ba99
sabbir <sabbir00921@gmail.com>
2026-01-09 00:03:47 +0600

    Fix asyncHandler and instrument

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,8 +1,37 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
+import fs from 'fs';
 
 const catchAsync = (fn: RequestHandler) => {
-      return (req: Request, res: Response, next: NextFunction) => {
-            Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+      return async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                  await fn(req, res, next);
+            } catch (error) {
+                  //! Don't modify the code below
+                  // AUTO DELETE UPLOADED FILES ON ERROR
+                  const files: Express.Multer.File[] = [];
+
+                  // upload.single()
+                  if (req.file) files.push(req.file);
+
+                  // upload.array() or upload.fields()
+                  if (req.files) {
+                        if (Array.isArray(req.files)) {
+                              files.push(...req.files);
+                        } else {
+                              Object.values(req.files).forEach((arr) => {
+                                    files.push(...arr);
+                              });
+                        }
+                  }
+
+                  // delete from local storage
+                  files.forEach((file) => {
+                        fs.unlink(file.path, (err) => {
+                              if (err) console.error('File cleanup failed:', err);
+                        });
+                  });
+                  next(error);
+            }
       };
 };
 
