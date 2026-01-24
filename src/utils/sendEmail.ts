131c7eaa3c3a16f53608ee132e93f6430898dfe8
sabbir <sabbir00921@gmail.com>
2026-01-25 01:39:46 +0600

    fix nodemailer mailer for production

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -10,17 +10,15 @@ interface MailerOptions {
       template: string;
       email: string;
 }
-// // Create transporter
+
+// Create transporter
 const transporter: Transporter = nodemailer.createTransport({
       host: config.brevo.host,
       port: config.brevo.port,
-      secure: false, // Always false for port 587
+      secure: config.brevo.port === 465 ? true : false, // Always false for port 587
       auth: {
             user: config.brevo.auth.user,
             pass: config.brevo.auth.pass,
-      },
-      tls: {
-            rejectUnauthorized: false,
       },
 });
 
@@ -28,15 +26,15 @@ const transporter: Transporter = nodemailer.createTransport({
 export const mailer = async ({ subject, template, email }: MailerOptions): Promise<void> => {
       try {
             const info = await transporter.sendMail({
-                  from: `Piano Academy <sabbir.dev001@gmail.com>`,
+                  from: `${config.brevo.senderName} <${config.brevo.senderEmail}>`,
                   to: email,
                   subject,
                   html: template,
             });
 
-            // console.log('📧 Email sent successfully:', info.messageId);
+            config.env === 'development' && console.log('Email sent successfully:', info.messageId);
       } catch (error) {
-            // console.error('❌ Failed to send email', error);
+            config.env === 'development' && console.error('Failed to send email', error);
             throw new AppError(500, 'Failed to send email', error);
       }
 };
